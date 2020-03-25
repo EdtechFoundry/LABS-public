2416e3c275fd4926d7b523248e89a1d549e3c947
ginccc <gregor@jarisch.net>
2020-03-25 12:46:57 +0100

    init commit

---
+++ precache-manifest.95b0665d27b80850ee6117fc350317f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef75f419321d7bc03fa80038518faf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "249e77f7d231f9e50de5",
+    "url": "/static/css/main.ea63033e.chunk.css"
+  },
+  {
+    "revision": "612550db12ee283f7164",
+    "url": "/static/js/2.155b11da.chunk.js"
+  },
+  {
+    "revision": "6dea14eab83053243865ab6f2019f727",
+    "url": "/static/js/2.155b11da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249e77f7d231f9e50de5",
+    "url": "/static/js/main.25de320f.chunk.js"
+  },
+  {
+    "revision": "6361d8e6431ae75a483d",
+    "url": "/static/js/runtime-main.ce2e477c.js"
+  },
+  {
+    "revision": "cc9a6a1a7ff1961996f4b25d3632ce49",
+    "url": "/static/media/bot_green_heart.cc9a6a1a.png"
+  }
+]);
